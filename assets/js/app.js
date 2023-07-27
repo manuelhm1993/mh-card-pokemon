8cffbb6b0131b5e43449edd503608c8ec018d81d
Manuel Henriquez <manuelhm1993@gmail.com>
2023-07-26 22:00:59 -0400

    Se hizo otro request fetch para capturar la imagen del pokemon en blob y se usa el data.json para llenar el template

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,18 +10,48 @@ const devolverNumeroPokemon = (min, max) => {
 };
 
 const pintarPokemon = (data) => {
+    const clonTemplateCard = templateCard.firstElementChild.cloneNode(true);
+
+    //------------------------- Hace el efecto capitalize, pone la primera letra en mayúscula
+    const nombrePokemon = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+
+    //------------------------- Hace el efecto capitalize, pone la primera letra en mayúscula
+    const tituloCard = clonTemplateCard.querySelector('.card .card-body .card-body-title');
+    const experiencia = tituloCard.querySelector('span');
+
+    tituloCard.textContent  = nombrePokemon;
+    experiencia.textContent = data.base_experience;
+
+    fetchBlob(data.sprites.other.dream_world.front_default, clonTemplateCard);
     
+    tituloCard.appendChild(experiencia);
+    fragment.appendChild(clonTemplateCard);
+    mainContainer.appendChild(fragment);
 };
 
 const fetchData = (url, numeroPokemon) => {
     //------------------------- Hace la petición a la API
     fetch(url + numeroPokemon)
-    //------------------------- Recibe la respuesta y la transforpa en JSON
+    //------------------------- Recibe la respuesta y la transforma en JSON
     .then(res => res.json())
     //------------------------- Toma la respuesta en JSON y le da el tratamiento pertinente
     .then(data => pintarPokemon(data))
     //------------------------- Control de errores
     .catch(err => console.log(`Error al cargar el pokemon: ${ err.message }`));
+};
+
+const fetchBlob = (url, template) => {
+    //------------------------- Hace la petición a la API
+    fetch(url)
+    //------------------------- Recibe la respuesta y la transforma en BLOB
+    .then(res => res.blob())
+    //------------------------- Toma la respuesta en BLOB y construye una url de imagen
+    .then(data => {
+        const objectURL = URL.createObjectURL(data);
+        template.querySelector('.card .card-body .card-body-img').src = objectURL;
+    })
+    //------------------------- Control de errores
+    .catch(err => console.log(err.message));
 };
 
 //------------------------- Eventos
